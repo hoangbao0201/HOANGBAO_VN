000c686919bd6e7e604b69fb0365271d78153c7b
hoangbao0201 <hoangbao020103@gmail.com>
2023-12-04 13:57:00 +0700

    Page Blog Detail

---
+++ src/app/(main)/blog/[slugBlog]/page.tsx
@@ -0,0 +1,53 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+
+import blogService, { GetBlogDetailProps } from "@/lib/services/blog.service";
+import SkeletonCardBlog from "@/components/modules/skeletons/SkeletonCardBlog";
+import SidebarLeftBlogDetail from "@/components/modules/Blog/SideLeftBlogDetail";
+import SidebarRightBlogDetail from "@/components/modules/Blog/SideRightBlogDetail";
+import ContentBlogDetail from "@/components/modules/Blog/ContentBlogDetail";
+
+type Props = {
+    params: { slugBlog: string }
+}
+const BlogDetailPage = async ({ params } : Props) => {
+
+    const { success, blog } = await blogService.getBlogDetail(params.slugBlog);
+
+    return (
+        <>
+            {/* {blog && (
+                <BlogSeo
+                    title={`${blog.title} - ${siteMetadata.title}`}
+                    thumbnail={blog.thumbnailUrl}
+                    blogImages={blog.blogImages}
+                    summary={blog.summary}
+                    author={blog.author.name}
+                    canonicalUrl={`${siteMetadata.siteUrl}/blog/${blog.slug}-${blog.blogId}`}
+                    createdAt={blog.createdAt}
+                    updatedAt={blog.updatedAt}
+                />
+            )} */}
+            <main className="max-w-7xl w-full min-h-screen mx-auto mb-4">
+                <div className="grid grid-cols-12">
+                    <div className="col-span-1 xl:block hidden pt-3">
+                        <SidebarLeftBlogDetail />
+                    </div>
+    
+                    <div className="lg:col-span-8 col-span-full pt-3">
+                        {blog ? (
+                            <ContentBlogDetail blog={blog} />
+                        ) : (
+                            <SkeletonCardBlog count={3} />
+                        )}
+                    </div>
+    
+                    <div className="xl:col-span-3 lg:col-span-4 col-span-full pt-3">
+                        <SidebarRightBlogDetail />
+                    </div>
+                </div>
+            </main>
+        </>
+    );
+};
+
+export default BlogDetailPage;
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
-export const authOptions = {
+const authOptions: NextAuthOptions = {
     providers: [
         GithubProvider({
             clientId: process.env.NEXT_PUBLIC_GITHUB_ID as string,
@@ -11,4 +11,8 @@ export const authOptions = {
     secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
 };
 
-export default NextAuth(authOptions);
+// export default NextAuth(authOptions);
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
